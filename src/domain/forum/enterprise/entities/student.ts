582418c60b93c3013bc1cdedca67d46fe1063a50
LetsWorkb1 <jhow.win@gmail.com>
2023-09-18 22:40:53 -0300

    Feat: Create encrypte and students use cases

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,23 @@ import { UniqueEntityID } from '@/core/entitites/unique-entity-id'
 
 interface StudendProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudendProps> {
+  get name() {
+    return this.name
+  }
+
+  get email() {
+    return this.email
+  }
+
+  get password() {
+    return this.password
+  }
+
   static create(props: StudendProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
 
